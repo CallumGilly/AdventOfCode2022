b0898ab8d6d1c8fcbf70db5f6d34c466e61e5273
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2022-12-04 11:06:47 +0000

    Day 4

---
+++ D4C1/main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    dupsum = 0
+    with open("input.txt", "r") as file:
+        list = file.readlines()
+    for split_pair in list:
+        split_pair = split_pair.split(",")
+        print(split_pair)
+
+        if int(split_pair[0].split("-")[0]) <= int(split_pair[1].split("-")[0]) and int(split_pair[0].split("-")[1]) >= int(split_pair[1].split("-")[1]):
+            dupsum += 1
+        elif int(split_pair[1].split("-")[0]) <= int(split_pair[0].split("-")[0]) and int(split_pair[1].split("-")[1]) >= int(split_pair[0].split("-")[1]):
+            dupsum += 1
+    print(dupsum)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
