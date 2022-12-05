1887914093faa4dcfec68ef1db62df0302d19169
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2022-12-05 10:55:55 +0000

    Day 5

---
+++ D5C2/main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+stack = []
+
+
+def main():
+    with open("input.txt", "r") as file:
+        input = file.readlines()
+    for col in range(1, 34, 4):
+        rowarr = []
+        for row in range(0, 8):
+            if input[row][col] != " ":
+                rowarr.append(input[row][col])
+        rowarr.reverse()
+        stack.append(rowarr)
+    flag = 0
+    for line in input:
+        if flag == 0:
+            if line == "\n":
+                flag = 1
+        else:
+            line_arr = line.split(" ")
+            move(int(line_arr[3]) - 1, int(line_arr[5]) - 1, int(line_arr[1]))
+    out = ""
+    for col in stack:
+        out += col.pop()
+    print(out)
+
+
+def move(col1, col2, count):
+    temp_stack = []
+    for x in range(0, count):
+        temp_stack.append(stack[col1].pop())
+    for x in range(0, count):
+        stack[col2].append(temp_stack.pop())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
